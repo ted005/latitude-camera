ca3927e256550043c258e1e26bb46f0aaa7dcf2c
ted005 <witwiky@me.com>
2015-04-07 22:52:06 +0800

    initial version of latitude life

---
+++ Latitude Life/Latitude Life/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Latitude Life
+//
+//  Created by Robbie on 15/4/7.
+//  Copyright (c) 2015年 Mattie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
