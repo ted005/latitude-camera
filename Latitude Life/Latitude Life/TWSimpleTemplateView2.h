f17b7234bf03e93072331f6287f207009ec8d51e
ted005 <witwiky@me.com>
2015-04-23 21:48:58 +0800

    add the 2nd template

---
+++ Latitude Life/Latitude Life/TWSimpleTemplateView2.h
@@ -0,0 +1,13 @@
+//
+//  TWSimpleTemplateView2.h
+//  Latitude Life
+//
+//  Created by Robbie on 15/4/23.
+//  Copyright (c) 2015年 Mattie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWSimpleTemplateView2 : UIView
+
+@end
---
+++ Latitude Life/Latitude Life/TWSimpleTemplateView2.m
@@ -0,0 +1,21 @@
+//
+//  TWSimpleTemplateView2.m
+//  Latitude Life
+//
+//  Created by Robbie on 15/4/23.
+//  Copyright (c) 2015年 Mattie. All rights reserved.
+//
+
+#import "TWSimpleTemplateView2.h"
+
+@implementation TWSimpleTemplateView2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
